cdf8ac2c846f43b0670e4f8de5a153b6933f461f
Aldianfp <aldianfarhanputra@gmail.com>
2025-04-09 20:35:37 +0700

    test-git.txt

---
+++ index.js
@@ -0,0 +1,16 @@
+let books = [
+    "The Great Gatsby",
+    "To Kill a Mockingbird",
+    "The Catcher in the Rye",
+];
+
+console.log(books);
+
+let products = [
+    {name: "Laptop", price: 1000},  
+    {name: "Mobile", price: 500},
+    {name: "Tablet", price: 700},
+]
+
+console.log('Ini list produk buku',products);
+console.log('Ini list buku',books);
